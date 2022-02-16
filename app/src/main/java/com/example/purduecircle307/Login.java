539cb5f4a24153fda53f5621632bf72fbe556691
Sriya Chundi <chundi@purdue.edu>
2022-02-16 16:11:13 -0500

    Navigation from Login to SignUp

--- app/src/main/java/com/example/purduecircle307/Login.java
+++ app/src/main/java/com/example/purduecircle307/Login.java
@@ -2,13 +2,40 @@ package com.example.purduecircle307;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    private Button LoginButton;
+    private EditText UserEmail;
+    private EditText UserPassword;
+    private Button NeedNewAccountLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        NeedNewAccountLink = (Button) findViewById(R.id.Login_signUpButton);
+        UserEmail = (EditText) findViewById(R.id.login_emailAddress);
+        UserPassword = (EditText) findViewById(R.id.login_Password);
+        LoginButton = (Button) findViewById(R.id.LoginButton);
+
+        NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendUserToSignUpActivity();
+            }
+        });
+    }
+
+    private void SendUserToSignUpActivity() {
+        Intent signUpIntent = new Intent(Login.this, SignUp.class);
+        startActivity(signUpIntent);
+        finish();
     }
 }
--- app/src/main/java/com/example/purduecircle307/MainActivity.java
+++ app/src/main/java/com/example/purduecircle307/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.purduecircle307;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    //Checks authentication of user
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //If user does not exist, navigate to sign up/log in page
+        if (currentUser == null) {
+            SendUserToLoginActivity();
+        }
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this, Login.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
+        finish();
     }
 }
