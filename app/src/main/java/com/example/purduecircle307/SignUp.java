e70ade5eb3354eecf3626a59badd43daf423a372
Aayushi Neema <neema.aayushi@gmail.com>
2022-02-16 23:23:56 -0500

    functionality to create account in firebase

--- app/src/main/java/com/example/purduecircle307/SignUp.java
+++ app/src/main/java/com/example/purduecircle307/SignUp.java
@@ -1,10 +1,20 @@
 package com.example.purduecircle307;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
 
@@ -12,15 +22,69 @@ public class SignUp extends AppCompatActivity {
     private EditText UserPassword;
     private EditText UserConfirmPassword;
     private Button CreateAccountButton;
+    private ProgressDialog loadingBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mAuth = FirebaseAuth.getInstance();
+
         UserEmail = (EditText) findViewById(R.id.signUp_email);
         UserPassword = (EditText) findViewById(R.id.signUp_password);
         UserConfirmPassword = (EditText) findViewById(R.id.signUp_confirmPassword);
         CreateAccountButton = (Button) findViewById(R.id.signUp_createAccount);
+        loadingBar = new ProgressDialog(this);
+
+        CreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNewAccount();
+            }
+        });
+    }
+
+    private void createNewAccount() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+        String confirmPassword = UserConfirmPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please include your email", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please include your password", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(confirmPassword)) {
+            Toast.makeText(this, "Please confirm your password", Toast.LENGTH_SHORT).show();
+        }
+        else if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loadingBar.setTitle("Creating new account");
+            loadingBar.setMessage("Loading. Please wait as your account is being created");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                Toast.makeText(SignUp.this, "Authentication was successful!", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(SignUp.this, "Error: " + message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
     }
 }
