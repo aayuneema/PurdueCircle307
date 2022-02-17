8164f5b72dbb6969479434a20a5c343c11334dc7
Sriya Chundi <chundi@purdue.edu>
2022-02-16 19:10:15 -0500

    Functions for SignUp page

--- app/src/main/java/com/example/purduecircle307/SignUp.java
+++ app/src/main/java/com/example/purduecircle307/SignUp.java
@@ -3,12 +3,24 @@ package com.example.purduecircle307;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+
+    private EditText UserEmail;
+    private EditText UserPassword;
+    private EditText UserConfirmPassword;
+    private Button CreateAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        UserEmail = (EditText) findViewById(R.id.signUp_email);
+        UserPassword = (EditText) findViewById(R.id.signUp_password);
+        UserConfirmPassword = (EditText) findViewById(R.id.signUp_confirmPassword);
+        CreateAccountButton = (Button) findViewById(R.id.signUp_createAccount);
     }
 }
