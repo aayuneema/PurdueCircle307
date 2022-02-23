87f34fa84b47c784fba130d83273cf0e06aefeab
ss-mathew <mathew64@purdue.edu>
2022-02-23 08:05:48 -0500

    posting - add image - add text - nav bar - button to post(functionality needed)

--- app/src/main/java/com/example/purduecircle307/PostActivity.java
+++ app/src/main/java/com/example/purduecircle307/PostActivity.java
@@ -1,14 +1,70 @@
 package com.example.purduecircle307;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.util.Objects;
 
 public class PostActivity extends AppCompatActivity {
+
+    //private Toolbar mToolbar;
+    private ImageButton SelectPostImage;
+    private Button UpdatePostButton;
+    private EditText PostDescription;
+    private static final int Gallery_Pick = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        SelectPostImage =(ImageButton) findViewById(R.id.select_post_image);
+        UpdatePostButton = (Button) findViewById(R.id.update_post_button);
+        PostDescription = (EditText) findViewById(R.id.post_description);
+/*
+
+        mToolbar = (Toolbar) findViewById(R.id.update_post_page_toolbar);
+        setSupportActionBar(mToolbar);
+        //Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Update Post");
+*/
+        SelectPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenGallery();
+            }
+        });
+    }
+
+    private void OpenGallery() {
+        Intent galleryIntent = new Intent();
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, Gallery_Pick);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            SendUserToMainActivity();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void SendUserToMainActivity() {
+        Intent mainIntent = new Intent(PostActivity.this, MainActivity.class);
+        startActivity(mainIntent);
     }
 }
