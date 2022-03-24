4c86dd624a8609a2a6c47e3fc5ac1e7bca68aa0b
Aayushi Neema <neema.aayushi@gmail.com>
2022-03-24 19:30:55 -0400

    added tag functionality

---
+++ app/src/main/java/com/example/purduecircle307/BrowseTagsActivity.java
@@ -0,0 +1,14 @@
+package com.example.purduecircle307;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BrowseTagsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_tags);
+    }
+}
