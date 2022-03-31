fc505f4e1caa4bc86a457bb38d3ac2ee12dea039
Rohan Purandare <rpuranda@purdue.edu>
2022-03-31 00:02:08 -0400

    created sendFriendTest and completed AreFriendsTest

---
+++ app/src/androidTest/java/com/example/purduecircle307/SendFriendRequestTest.java
@@ -0,0 +1,71 @@
+package com.example.purduecircle307;
+
+import static org.junit.Assert.assertEquals;
+
+import androidx.annotation.NonNull;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.Map;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class SendFriendRequestTest {
+
+    private DatabaseReference FriendRequestRef;
+
+    @Before
+    public void setup() {
+        FriendRequestRef = FirebaseDatabase.getInstance().getReference().child("FriendRequests");
+    }
+
+    //Test for a user sending a request
+    @Test
+    public void sendRequest() {
+        String senderID = "";
+        String receiverID = "";
+        FriendRequestRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean works = false;
+                Map<String, Object> users = (Map<String, Object>) snapshot.getValue();
+                for (Map.Entry<String, Object> entry : users.entrySet()) {
+                    if (senderID.equals(entry.getKey())) {
+                        Map<String, String> requesters = (Map) entry.getValue();
+                        for (Map.Entry<String, String> entry1 : requesters.entrySet()) {
+                            if (receiverID.equals(entry1.getKey())) {
+                                works = true;
+                            }
+                        }
+                    }
+                }
+                assertEquals(true, works);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    //Test for a user receiving a request
+    @Test
+    public void receiveRequest() {
+
+    }
+
+    //Test for users that are friends
+    @Test
+    public void alreadyFriends() {
+
+    }
+}
